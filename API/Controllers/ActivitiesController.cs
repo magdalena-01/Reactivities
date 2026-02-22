f9ef230affc7f192613ff1c9b086989b972c02dc
Magdalena <magdalenamagde2703@gmail.com>
2026-02-22 18:03:00 +0100

    Initial commit

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,33 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+
+namespace API.Controllers;
+
+public class ActivitiesController(AppDbContext context): BaseApiController
+{
+   // private readonly AppDbContext context; //through methods inside this class, refers to data, files, or memory that can be accessed and read but not modified, edited, or deleted.
+    
+   // public ActivitiesController(AppDbContext context)
+   // {
+    //    this.context = context;
+    //}
+
+    [HttpGet]
+    
+    public async Task<ActionResult<List<Activity>>> GetActivities()
+    {
+        return await context.Activities.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Activity>> GetActivityDetail(string id)
+    {
+        var activity = await context.Activities.FindAsync(id);
+        
+        if(activity == null) return NotFound(); 
+        return activity;
+    }
+}
---
+++ API/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<AppDbContext>(); //access to dbcontext
+    await context.Database.MigrateAsync(); //create db and apply the migration
+    await DbInitializer.SeedData(context);
+}
+catch (Exception e)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(e, "An error occurred seeding the DB.");
+}
+app.Run();
