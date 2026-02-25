dd8b8f1b50f6879b984a22a8774bc9c130b1b416
Magdalena <magdalenamagde2703@gmail.com>
2026-02-25 15:16:19 +0100

    New changes - adding React

---
+++ client/src/lib/types/index.d.ts
@@ -0,0 +1,12 @@
+﻿export interface Activity {
+    id: string;
+    title: string;
+    date: string;
+    description: string;
+    category: string;
+    isCanceled: boolean;
+    city: string;
+    venue: string;
+    latitude: number;
+    longitude: number;
+}
